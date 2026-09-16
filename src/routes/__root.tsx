a3d9e0be84a57e8ca262121a27ac3cb4638369a6
lunabarrozonutricion-blip <lunabarrozonutricion@gmail.com>
2026-09-16 18:14:16 -0300

    Actualizar cache a v5 para nuevo informe grupal

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -13,7 +13,7 @@ import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 
 const OFFLINE_CACHE_NAME =
-  "san-lorenzo-antro-v4";
+  "san-lorenzo-antro-v5";
 
 async function cacheLoadedAssets() {
   if (
