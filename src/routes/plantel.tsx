64d59c93cfbeef031adba1a15112d7e34372637c
lunabarrozonutricion-blip <lunabarrozonutricion@gmail.com>
2026-09-17 22:00:36 -0300

    Conectar plantel con objetivos

--- src/routes/plantel.tsx
+++ src/routes/plantel.tsx
@@ -23,7 +23,11 @@ import {
   fmtDiff,
   metricValue,
 } from "@/lib/calc";
-import { useControls, usePlayers } from "@/lib/hooks";
+import {
+  useControls,
+  useObjectivePeriods,
+  usePlayers,
+} from "@/lib/hooks";
 import { best2025ForPlayer } from "@/lib/historical-best-2025";
 import {
   GROUP_LABELS,
