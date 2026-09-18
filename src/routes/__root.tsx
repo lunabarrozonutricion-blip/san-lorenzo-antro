824f751de8b00847e84b396c4ffefb75a1b9dcce
lunabarrozonutricion-blip <lunabarrozonutricion@gmail.com>
2026-09-18 17:41:39 -0300

    Actualizar hidratacion y cache offline a v12

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -13,7 +13,7 @@ import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 
 const OFFLINE_CACHE_NAME =
-  "san-lorenzo-antro-v11";
+  "san-lorenzo-antro-v12";
 
 async function cacheLoadedAssets() {
   if (
