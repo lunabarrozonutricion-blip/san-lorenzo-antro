d4045a5e8154504fa4573a182b1c907b10cb1ae4
lunabarrozonutricion-blip <lunabarrozonutricion@gmail.com>
2026-09-18 10:21:15 -0300

    Actualizar cache de app a v11

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -13,7 +13,7 @@ import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 
 const OFFLINE_CACHE_NAME =
-  "san-lorenzo-antro-v10";
+  "san-lorenzo-antro-v11";
 
 async function cacheLoadedAssets() {
   if (
